9233a27baf8c735c548c46f23c743208908d30c7
PC JOSE FERNANDO <josefernandojosefernando.09@gmail.com>
2025-11-26 21:23:02 -0300

    first commit

---
+++ src/main/java/com/POO/Sistema_De_Bar/SistemaDeBarApplication.java
@@ -0,0 +1,13 @@
+package com.POO.Sistema_De_Bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/POO/Sistema_De_Bar/SistemaDeBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POO.Sistema_De_Bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
